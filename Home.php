7dba7bd85d6935c860dd71c58abe01f56971283b
Mathusha07 <si2ma4@gmail.com>
2022-07-23 22:14:51 +0530

    HomePage

--- Home.php
+++ Home.php
@@ -1,6 +1,6 @@
 <?php include_once('script.php');
-include_once('config.php');
-?>
+include_once('config.php'); 
+?> <!--"include_once" use for connect files with other files -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,66 +10,19 @@ include_once('config.php');
     <title>Home</title>
 </head>
 <body>    
-<div class="container">
-  <div class="row">
-  <div class="col-sm-12">
-<nav class="navbar navbar-light" style="background-color:#1A75B1;">
-  <a class="navbar-brand" href="#">&nbsp &nbsp 
-  <p class="float-right">
-    <span style="font-family:Tekton Pro;font-size:30px;">SIGN UP</span>
-    <span style="font-family:Tekton Pro;font-size:30px;">LOGIN</span>
-    <br>
-</p>
-</div>
-</div>
-<div class="row">
-<div class="col-sm-12">
-    <img src="img/sl-tours-logo-1-removebg-preview.png" width="30" height="40" class="d-inline-block align-top" alt="">&nbsp &nbsp &nbsp
-    
-    <span style="font-family:showcard gothic;font-size:30px;">SRILANKA TOUR</span>
-  </a>
-  </div>
-  </div>
-  </nav>
-  <div class="row"><div class="col-sm-12">
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#1A75B1;">
-
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Home </span><span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span style="font-family:Tahomt;">About Us</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Book</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="#"><span style="font-family:Tahomt;">Packages</span></a>
-      </li>
-      <a class="nav-link active" href="#"><span style="font-family:Tahomt;">Contacts</span></a>
-      </li>
-    </ul>
-  </div>
-  </div>
-  </div>
-</nav>
-<div class="card bg-light text-dark">
-  <img class="card-img-top" src="img/Cheap-couple-vacation-in-Sri-Lanka.jpg" alt="Card image cap">
+<?php include_once('navbar.php');?> 
+<div class="card bg-light text-dark"> 
+  <img class="card-img-top" src="img/Cheap-couple-vacation-in-Sri-Lanka.jpg" alt="Card image cap"> <!--Baground image-->
   <div class="card-img-overlay">
     <br><br><br><br>
     <center>
-  <span style="font-family:Trajan Pro 3;font-size:75px;">Welcome to Srilanka</span><br><br>
-    <span style="font-family:Segoe Print;font-size:70px;">Available Tour Search</span>
+  <span style="font-family:Trajan Pro 3;font-size:75px;">Welcome to Srilanka</span><br><br> <!--Image Over Text-->
+    <span style="font-family:Segoe Print;font-size:70px;">Available Tour Search</span> <!--Image Over Text-->
     <br><br>
-    <a href="#" class="btn btn-light">Search</a>
+    <a href="#" class="btn btn-light">Search</a> <!--Search Button-->
 </center>
 </div>
 </div>
-<div class="row"><div class="col-sm-12">
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#100F0F;">
-<div class="collapse navbar-collapse" id="navbarNav">
-</nav>
+<?php include_once('nav.php');?>
 </body>
 </html>
