bb52b6e71534fc3156d4dd2b548c004d84b5c5a1
Mathusha07 <si2ma4@gmail.com>
2022-07-23 22:19:33 +0530

    Footer Page

--- nav.php
+++ nav.php
@@ -1,64 +1,8 @@
-<?php include_once('script.php');
-include_once('config.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>    
-<nav class="navbar navbar-light" style="background-color:#1A75B1;">
-  <a class="navbar-brand" href="#">&nbsp &nbsp 
-  <P style="float:right;"> 
-    <span style="font-family:Tekton Pro;font-size:30px;">SIGN UP</span>
-    <span style="font-family:Tekton Pro;font-size:30px;">LOGIN</span>
-    <br>
-</P>
-    <img src="img/sl-tours-logo-1-removebg-preview.png" width="30" height="40" class="d-inline-block align-top" alt="">&nbsp &nbsp &nbsp
-    
-    <span style="font-family:showcard gothic;font-size:30px;">SRILANKA TOUR</span>
-  </a>
-  </nav>
-
-
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#1A75B1;">
-
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Home </span><span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span style="font-family:Tahomt;">About Us</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Book</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="#"><span style="font-family:Tahomt;">Packages</span></a>
-      </li>
-      <a class="nav-link active" href="#"><span style="font-family:Tahomt;">Contacts</span></a>
-      </li>
-    </ul>
-  </div>
+<?php include_once('script.php');?>
+<div class="row"> <!--Footer row start-->
+<div class="col-sm-12">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#100F0F;">
+<div class="collapse navbar-collapse" id="navbarNav">
+</div>
+</div>  <!--Footer row end-->
 </nav>
-
-
-
-<div class="card bg-light text-dark">
-  <img class="card-img-top" src="img/Cheap-couple-vacation-in-Sri-Lanka.jpg" alt="Card image cap">
-  <div class="card-img-overlay">
-    <br><br><br><br>
-    <center>
-  <span style="font-family:Trajan Pro 3;font-size:75px;">Welcome to Srilanka</span><br><br>
-    <span style="font-family:Segoe Print;font-size:70px;">Available Tour Search</span>
-    <br><br>
-    <a href="#" class="btn btn-light">Search</a>
-</center>
-</div>
-</div>
-</body>
-</html>
