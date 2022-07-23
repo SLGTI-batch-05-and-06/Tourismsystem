c4edf9363a5f4c1ff08b3d810e0df5c1aa10abac
Mathusha07 <si2ma4@gmail.com>
2022-07-23 22:26:18 +0530

    Navigation Bar

---
+++ navbar.php
@@ -0,0 +1,53 @@
+
+<?php include_once('script.php');?>
+<div class="container">
+
+  <div class="row"> <!--SIGN UP & LoGIN row start-->
+  <div class="col-sm-12">
+<nav class="navbar navbar-light" style="background-color:#1A75B1;">
+  <a class="navbar-brand" href="#">&nbsp &nbsp 
+  <p class="float-right">
+    <span style="font-family:Tekton Pro;font-size:30px;">SIGN UP</span>
+    <span style="font-family:Tekton Pro;font-size:30px;">LOGIN</span>
+    <br>
+</p>
+</div>
+</div> <!--SIGN UP & LoGIN row end-->
+
+
+<div class="row"> <!--Lion image & SRILANKA TOUR row start-->
+<div class="col-sm-12">
+    <img src="img/sl-tours-logo-1-removebg-preview.png" width="30" height="40" class="d-inline-block align-top" alt="">&nbsp &nbsp &nbsp
+    
+    <span style="font-family:showcard gothic;font-size:30px;">SRILANKA TOUR</span>
+  </a>
+  </div>
+  </div> <!--Lion image & SRILANKA TOUR row end-->
+  
+</nav>
+  
+<div class="row"> <!--Files link row start-->
+<div class="col-sm-12">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#1A75B1;">
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Home </span><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><span style="font-family:Tahomt;">About Us</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Book</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#"><span style="font-family:Tahomt;">Packages</span></a>
+      </li>
+      <a class="nav-link active" href="#"><span style="font-family:Tahomt;">Contacts</span></a>
+      </li>
+    </ul>
+  </div>
+  </div> <!--Files link row end-->
+  </div> <!--Div Container close-->
+</nav>
