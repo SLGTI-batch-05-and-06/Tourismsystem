1ce59fc0ec6806d2f5b8640b0f00a93f0deaac19
Mathusha <106523123+Mathusha07@users.noreply.github.com>
2022-07-24 00:50:39 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,59 @@
+
+<?php include_once('script.php');?>
+<div class="container">
+<nav class="navbar-light" style="background-color:#1A75B1;">
+  <div class="row"> 
+  
+  <div class="col">
+      <!--coloum divide 3-->
+    </div>
+    <div class="col-6">
+     <!--coloum divide 6-->
+    </div>
+  <div class="col"> <!--SIGN UP & LoGIN row start-->
+    <!--coloum divide 3-->
+   
+  <div class="float-lg-right"> <nav class="navbar navbar-expand-lg">
+    <span style="font-family:Tekton Pro;font-size:25px;">SIGN UP</span>&nbsp &nbsp
+    <span style="font-family:Tekton Pro;font-size:25px;">LOGIN</span>&nbsp &nbsp
+    <br></nav>
+</div>
+</div>
+</div> <!--SIGN UP & LoGIN row end-->
+
+<div class="row"> <!--Lion image & SRILANKA TOUR row start-->
+<div class="col-sm-12">&nbsp &nbsp
+    <img src="img/sl-tours-logo-1-removebg-preview.png" width="30" height="40" class="d-inline-block align-top" alt="">&nbsp &nbsp &nbsp
+    
+    <span style="font-family:showcard gothic;font-size:30px;">SRILANKA TOUR</span>
+  </a>
+  </div>
+  </div> <!--Lion image & SRILANKA TOUR row end-->
+  
+</nav>
+
+<div class="row"> <!--Files link row start-->
+<div class="col-sm-12">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#1A75B1;">
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Home </span><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span style="font-family:Tahomt;">About Us</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span style="font-family:Tahomt;">Book</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#"><span style="font-family:Tahomt;">Packages</span></a>
+      </li>
+      <a class="nav-link" href="#"><span style="font-family:Tahomt;">Contacts</span></a>
+      </li>
+    </ul>
+  </div>
+  </div> <!--Files link row end-->
+  </div> <!--Div Container close-->
+</nav>
